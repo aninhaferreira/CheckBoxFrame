fdae9d2d9864c6345056eea282b9368c7918ab96
aninhaferreira <124297002+aninhaferreira@users.noreply.github.com>
2023-03-23 14:50:35 -0300

    Add files via upload

---
+++ checkboxframe/CheckBoxTest.java
@@ -0,0 +1,21 @@
+
+package checkboxframe;
+
+import javax.swing.JFrame;
+
+
+
+public class CheckBoxTest
+{
+public static void main(String[] args)
+{
+Checkboxframe CheckBoxFrame = new Checkboxframe();
+CheckBoxFrame.setDefaultCloseOPeration(JFrame.EXIT_ON_CLOSE);
+CheckBoxFrame.setSize( 275, 100);
+CheckBoxFrame.setVisible(true);
+
+
+ }
+}
+    
+
